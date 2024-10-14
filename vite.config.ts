842874eb5f1000693d406aa224f5292ab99f3060
liangyong <liangyong9098@cvte.com>
2024-10-14 21:26:43 +0800

    chore: test

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import vitePluginImp from 'vite-plugin-imp';
 
 // https://vitejs.dev/config/
 export default defineConfig({
- base: '/docs/',
   plugins: [
     react(),
     vitePluginImp({
@@ -28,8 +27,7 @@ export default defineConfig({
     },
   },
   build: {
-   outDir: 'docs',
+   outDir: 'dist',
    assetsDir: 'assets', // 资源目录
-   // 设置输出js/css路径,统一携带 /dos/前缀
   }
 })
